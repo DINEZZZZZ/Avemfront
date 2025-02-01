1fb127d8c61f9cfd1dfd0d32456a5a46698bfd23
=DINEZZZZZ <=dineshasus2023@gmail.com>
2025-02-01 22:36:51 +0530

    last commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/FrontendAvem/', // Add this line with your repository name
+  base: '/Avemfront/', // Add this line with your repository name
 })

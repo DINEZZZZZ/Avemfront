70643b4b288231c232786a65d561c8d1c99b6b7e
=DINEZZZZZ <=dineshasus2023@gmail.com>
2025-02-01 20:06:00 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import BoxList from "./components/Boxlists/BoxList";
 import More from "./components/more/More";
